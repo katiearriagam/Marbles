e67a03457ff560f064846b42a5b57a0facc62b7d
Katie Arriaga M <katiearriaga@live.com>
2017-10-11 18:12:37 -0500

    Adding all instruction blocks drafts

---
+++ UI/Marbles-ui/Marbles-ui/DoBlock.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Marbles_ui
+{
+    public sealed partial class DoBlock : UserControl
+    {
+        public DoBlock()
+        {
+            this.InitializeComponent();
+        }
+
+		private void Rectangle_Tapped(object sender, TappedRoutedEventArgs e)
+		{
+			FlyoutBase.ShowAttachedFlyout((FrameworkElement)sender);
+		}
+	}
+}
